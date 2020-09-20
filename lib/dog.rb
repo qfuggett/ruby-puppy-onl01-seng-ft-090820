925ca21de9088327da44914035332664c33c76e3
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 17:51:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  attr_accessor :puppy
+  @@all = [ ]  #storage for each puppy born/each instance
+  
+  def initialize(puppy)
+    @puppy = puppy
+    @@all << save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    binding.pry
+    @@all.each do |individual_dogs|
+      puts "#{individual_dogs}"
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
