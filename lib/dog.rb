15ba9cbcf99d07b82be0589672fae8621acb587d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 18:00:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  attr_accessor :puppy
+  @@all = [ ]  #storage for each puppy born/each instance
+  
+  def initialize(name)
+    @name = name
+    self.save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |individual_dogs|
+      puts individual_dogs.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def name
+    @name
+  end
+  
+end
